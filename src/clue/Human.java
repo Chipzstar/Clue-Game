619464ba4190a5292dcd8aa433bda33e2ea089c9
Chisom Oguibe <chipzstar.dev@gmail.com>
2019-04-11 15:08:50 +0100

    Update Human.java

--- src/clue/Human.java
+++ src/clue/Human.java
@@ -15,26 +15,81 @@ import java.util.Set;
  * @author xxlig
  */
 public class Human extends Player{
- 
+
     private Scanner input;
-    
+
     public Human(String name, Set<MurderCard> mCards, Tile t, Game g){
         this.name = name;
         this.mCards = mCards;
         this.position = t;
         this.immunueToSuggestion = false;
         Scanner input = new Scanner(System.in);
-    }  
-    
+    }
+
     private int getInput (int maxVal){
         int val= -1;
         while(val<0||val>maxVal){
             val = input.nextInt();
-            
+
         }
         return val;
     }
-       
+
+    public ArrayList<MurderCard> makeAccusation(){
+        ArrayList<MurderCard> accusation = new ArrayList<>();
+        ArrayList<MurderCard> temp = d.getWeaponMCards();
+
+        System.out.println("Weapon");
+        for(int i = 0; i<temp.size(); i++){
+            System.out.println(i +": " +temp.get(i).getName());
+        }
+
+        System.out.println("Selection: ");
+        accusation.add(temp.get(getInput(temp.size()-1)));
+        temp = d.getRoomMCards();
+
+        System.out.println("Room");
+        for(int i = 0; i<temp.size(); i++){
+            System.out.println(i +": " +temp.get(i).getName());
+        }
+        System.out.println("Selection: ");
+        accusation.add(temp.get(getInput(temp.size()-1)));
+        temp = d.getCharacterMCards();
+
+        System.out.println("Character");
+        for(int i = 0; i<temp.size(); i++){
+            System.out.println(i +": " +temp.get(i).getName());
+        }
+
+        System.out.println("Selection: ");
+        accusation.add(temp.get(getInput(temp.size()-1)));
+        return accusation;
+    }
+
+
+    public ArrayList<MurderCard> makeSuggestion(){
+        ArrayList<MurderCard> accusation = new ArrayList<>();
+        ArrayList<MurderCard> temp = d.getWeaponMCards();
+        System.out.println("Weapon");
+        for(int i = 0; i<temp.size(); i++){
+            System.out.println(i +": " +temp.get(i).getName());
+        }
+        System.out.println("Selection: ");
+        accusation.add(temp.get(getInput(temp.size()-1)));
+        //gets roomMCard of current room players in
+        temp.add(d.getRoomMCard(((RoomTile) this.position).getRoom().getName()));
+        temp = d.getCharacterMCards();
+        System.out.println("Character");
+        for(int i = 0; i<temp.size(); i++){
+            System.out.println(i +": " +temp.get(i).getName());
+        }
+        System.out.println("Selection: ");
+        accusation.add(temp.get(getInput(temp.size()-1)));
+        return accusation;
+    }
+
+
+
     //Reaveling cards- if any matches, select card to reveal
     public MurderCard answerSuggestion(ArrayList<MurderCard> suggestion){
         ArrayList <MurderCard> matches = new ArrayList<>();
@@ -47,22 +102,22 @@ public class Human extends Player{
             return null;
         }
         else{
-            if(immunueToSuggestion){                   
+            if(immunueToSuggestion){
                 immunueToSuggestion = false;
-                    return null;
-                } 
-                else
-                {
-                    //return random match, can be choosen, but will be implemented later
-                    System.out.println("Cards matched with suggestion are:");
-                    for(int i = 0; i<matches.size(); i++){
-                        System.out.println(i +"  "+matches.get(i).name + " "+ matches.get(i).getClass().getName());
-                    }
-                    System.out.println("What card do you want to reveal");
-                    getInput(matches.size()-1);
-                    return(matches.get(0));
+                return null;
             }
-        }  
-    } 
-    
+            else
+            {
+                //return random match, can be choosen, but will be implemented later
+                System.out.println("Cards matched with suggestion are:");
+                for(int i = 0; i<matches.size(); i++){
+                    System.out.println(i +"  "+matches.get(i).name + " "+ matches.get(i).getClass().getName());
+                }
+                System.out.println("What card do you want to reveal");
+                getInput(matches.size()-1);
+                return(matches.get(0));
+            }
+        }
+    }
+
 }
